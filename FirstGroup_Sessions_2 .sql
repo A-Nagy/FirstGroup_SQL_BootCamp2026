3f116376bd85a9b797a850dee8d5979b58317669
A-Nagy <70291196+A-Nagy@users.noreply.github.com>
2026-07-17 18:00:29 +0300

    Add files via upload
    Secand upload

---
+++ FirstGroup_Sessions_2 .sql
@@ -0,0 +1,182 @@
+--SQL ---DDl ---DML ---DQl 
+--data definition Lang
+-- Create   Alter    Drop 
+/*
+1. CREATE TABLE:
+----------------
+Syntax:
+    CREATE TABLE table_name (
+        column_name datatype [CONSTRAINT constraint_name constraint_type],
+        ...,   
+        [CONSTRAINT constraint_name constraint_type]
+    );
+
+Naming Rules:
+    - Maximum 128 characters.
+    - Can include A-Z, a-z, 0-9, _
+    - Must begin with a letter.
+    - Must not be a reserved keyword.
+
+Common Datatypes in SQL Server:
+    - Numeric: INT, BIGINT, SMALLINT, DECIMAL(p,s), FLOAT, REAL  -- decimal(6,1) 9999.99
+    - Text: CHAR(n), VARCHAR(n), TEXT
+    - Date/Time: DATE, DATETIME, SMALLDATETIME, TIME
+    - Other: BIT, MONEY, UNIQUEIDENTIFIER, VARBINARY(MAX)
+
+Constraint Types:
+    - PRIMARY KEY
+    - UNIQUE
+    - NOT NULL
+    - CHECK (condition)
+    - FOREIGN KEY REFERENCES other_table(column)
+
+2. ALTER TABLE:     ADD     Modify      Drop  :   column
+					ADD                 Drop  :   constraint 
+---------------
+Syntax Overview:
+ALTER TABLE table_name
+					   ADD column_name datatype [contraint Statement ];
+
+             -- Modify column datatype or nullability
+					   ALTER COLUMN column_name new_datatype ;
+			
+					   Drop  COLUMN column_name ; 
+
+			  -- Add a constraint
+			   ADD CONSTRAINT constraint_name constraint_type;
+			  -- Drop a constraint
+			   DROP CONSTRAINT constraint_name;
+
+3. DROP TABLE:
+--------------
+Syntax:
+    DROP TABLE table_name;
+Example:
+    DROP TABLE Employees;
+
+Note:
+    - SQL Server automatically removes constraints and Data with the table.
+
+*/
+create table emp1(
+emp_id   int , 
+emp_name varchar(200)
+);
+
+create table emp2(
+emp_id   int          primary key , 
+emp_name varchar(200) not null    ,
+phone    char(12)     unique        
+);
+
+create table emp3(
+emp_id   int          constraint emp3_emp_id_pk    primary key , 
+emp_name varchar(200) constraint emp3_emp_name_nk  not null ,
+phone    char(12)     constraint emp3_emp_phone_uk unique       
+);
+
+create table emp4(
+emp_id   int                                                , 
+emp_name varchar(200) constraint emp4_emp_name_nk  not null ,
+phone    char(12)                                           ,
+manager  int         
+constraint emp4_emp_id_pk    primary key(emp_id)            ,
+constraint emp4_emp_phone_uk unique(phone)
+);
+
+create table travels(
+trvel_num  int          ,
+trvel_date date         ,
+destantion varchar(20)  , 
+constraint pk_travels primary Key ( trvel_num,trvel_date)
+)
+
+create table travel_tikits(
+Tikit_Number  int       primary key    ,
+Tikit_date   date          ,
+tikit_type varchar(20)     , 
+trvel_num  int             ,
+trvel_date date            , 
+constraint fk_Tikit foreign key (trvel_num,trvel_date) references travels ( trvel_num,trvel_date) 
+)
+
+create table emp_Omar(
+emp_id   int                                                  ,
+emp_name varchar(200) constraint  empOmar_emp_name_nk not null,
+phone    char(12)                                             ,
+constraint empOmar_emp_id_pk primary key(emp_id),
+constraint empOmar_phone_uk  unique (phone)
+);
+
+alter table emp1 add email varchar(100) ;
+alter table emp2 add email varchar(100) not null  ;
+alter table emp3 add email varchar(100) constraint emp3_emp_email_nk not null  ;
+alter table emp4 add phone varchar(20) ;
+--------------------------------------------------------------
+alter table emp1 alter column email varchar(150) ;
+alter table emp2 alter column email varchar(100) null   ;
+alter table emp3 Alter column email varchar(200) not null ;
+--------------------------------------------------------------
+alter table emp1 Drop column email  ;
+alter table emp2 Drop column email  ;
+alter table emp3 Drop column email  ;
+alter table emp4 Drop column phone  ;
+--------------------------------------------------------------
+Alter Table emp4 Add  Constraint  emp4_emp_phone_uk unique(phone)
+--------------------------------------------------------------
+Alter table emp4 Drop constraint  emp4_emp_phone_uk ;
+--------------------------------------------------------------
+Drop table emp1;
+Drop table emp2;
+Drop table emp3;
+Drop table emp4;
+
+exec sp_helpconstraint 'emp4'
+-- ===========================
+-- My_Depts and My_Emps Setup
+-- ===========================
+
+--Q1 : Create Table MyDepts As Following : 
+   --	Dept_ID   int          --> primary Key
+	--	Dept_name varchar(100) --> not Null 
+create table MyDeptss( 
+Dept_ID   int          constraint dept_emp_id   primary key ,
+Dept_name varchar(200) constraint dept_emp_name not null 
+) ;
+
+--Q2 : Create Table MyEmps As Following : 
+	    --	Emp_ID    int           --> primary K
+		--Emp_name  varchar(150)  --> not Null 
+		--Salary    Deciml(8,2)   --> Check(salar
+		--hire_date Date 
+		--Dept_ID   int           --> Foreign key
+create table my_empss(
+	emp_id    int,
+	emp_name  varchar(100) constraint name_cons not null         ,
+	salary    decimal(8,2) check( salary between 4000 and 10000) ,
+	hire_date date default GetDate(),
+	dept_id   int,
+	constraint fk_emp foreign key(dept_id) references MyDeptss(dept_id)
+);
+
+-- -------------------- Questions ------------------------
+-- * create courses table as follow:
+--   cours_id => pk
+--   course_title => not null
+--   price => between 800 and 3000
+--   start_date => default value the date after 7 days
+--   duration => btw 12 and 120
+
+-- * create a projects table 
+--   project_id => pk
+--   project_name => not null
+--   client_name => 
+--   hour_rate => positive greater than 1
+
+-- * create a tasks table
+--   task_id => pk
+--   description => max char 255
+--   start_date => date
+--   end_date => greater than start_date
+--   project_id => foreign key
+
---
+++ FirstGroup_Sessions_3 .sql
@@ -0,0 +1,83 @@
+
+
+/*
+=====================================================
+Data Manipulation Language (DML) in SQL Server
+=====================================================
+Main Comands : insert     upadete     Delete 
+
+1. INSERT:
+----------
+Used to add new records to a table.
+
+Syntax:
+    INSERT INTO table_name (column1, column2, ...)
+				    VALUES (value1 , value2 , ...);
+2. UPDATE:
+----------
+Used to modify existing records.
+
+Syntax:
+    UPDATE table_name
+    SET column1 = value1, column2 = value2, ...
+    WHERE condition;
+
+3. DELETE:
+----------
+Used to remove one or more records.
+
+Syntax:
+    DELETE FROM table_name
+    WHERE condition;
+
+
+*/
+insert into mydeptss(dept_id , dept_name )
+			  values( 1       , 'It'     )
+
+insert into mydeptss(dept_id  , dept_name  ) --pk 
+			  values( 1       , 'FullStack')
+
+insert into mydeptss(dept_id  )--not null 
+			  values( 3       )
+
+insert into my_empss(emp_id , emp_name , salary )
+			 values ( 1     , 'ahmad'     , 4500)
+
+insert into my_empss( salary, emp_name , emp_id )
+			 values ( 5000  , 'ahmad'  , 1      )
+
+insert into my_empss( salary, emp_name , emp_id  , hire_date)
+			 values ( 5000  , 'ahmad'  , 1       , '2026-07-18' )
+
+--------------------------------------------------------
+
+update my_empss 
+    set  salary = 4000  , dept_id = 1
+   where salary <4000
+
+update mydeptss
+ set dept_name = 'CS'
+ where dept_id = 2
+
+--------------------------------------------------------
+ delete from my_empss
+
+ delete from mydeptss
+ where dept_id = 2
+
+-- update Myemps 
+--set hire_date = '2025-07-16', salary = 13000, Dept_ID = 3,Emp_name = 'Ahmed'
+--where Emp_ID = 1
+
+--insert into my_emp (emp_id,emp_name, salary, dept_id) values (101, 'ali',8000 ,1);
+--select * from my_emp
+
+--insert into MyEmps (Emp_ID,Emp_name,salary,hire_date,Dept_ID)
+--            values (44    ,'Omar'  ,15000 ,NULL     , 1     )
+
+
+
+select * from MyEmps
+select *  from MyDeptss
+select *  from my_empss
